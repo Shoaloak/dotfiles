f41b90bd957c0c71c901530326aded9b9ecea7f3
Shoaloak <axel.koolhaas@outlook.com>
2016-03-31 19:47:35 +0200

    vim-plugin fetcher added

---
+++ .vim/bundle/fetch-plugins.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# fetch plugins
+
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/altercation/vim-colors-solarized.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/nelstrom/vim-visual-star-search.git
